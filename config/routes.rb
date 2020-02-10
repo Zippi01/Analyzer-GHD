8d35fd5741b6493ece7f583852bac3e924e30f18
Artem <ayarem21@gmail.com>
2020-02-10 17:47:36 +0200

    Devise login

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  root 'home_pages#index'
+end
