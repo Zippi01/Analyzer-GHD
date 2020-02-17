b0d72b06285703e933944f7f23b11c42713ad85d
Zippi01 <katyakovtun080619@gmail.com>
2020-02-17 16:12:41 +0200

    you can use form to add url(work)

--- app/controllers/parser_sites_controller.rb
+++ app/controllers/parser_sites_controller.rb
@@ -2,13 +2,7 @@ class ParserSitesController < ApplicationController
   require 'open-uri'
   require 'nokogiri'
 
-  def index
-    # source = 'https://hard.rozetka.com.ua/kingston_kc_s44240_6f/p70512518/comments/'
-    # page = Nokogiri::HTML.parse(open(source))
-    # h3 = page.css('div.product-comment div.product-comment__inner div.product-comment__body p').each do |link|
-    #   puts link.content
-    # end
-  end
+  def index; end
 
   def new
     @parser_site = ParserSite.new
@@ -16,9 +10,11 @@ class ParserSitesController < ApplicationController
 
   def create
     source = parser_pages_params
-    page = Nokogiri::HTML.parse(open(source.to_s))
-    h3 = page.css('div.product-comment div.product-comment__inner div.product-comment__body p').each do |link|
-      puts link.content
+    page = Nokogiri::HTML.parse(open(source["url"]))
+    page.css('div.product-comment div.product-comment__inner div.product-comment__body p').each do |link|
+      # puts link.content
+      @parser_site = ParserSite.new(:comment => link.content.to_s, :url => source["url"])
+      @parser_site.save
     end
   end
 
