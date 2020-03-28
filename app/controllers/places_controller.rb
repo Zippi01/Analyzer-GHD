34b773df012569b09da91a9a0f26b9a9cc1713c2
Artem <ayarem21@gmail.com>
2020-03-28 17:13:59 +0200

    add map && parse data from map && nokogiri parse from google map(alpha)

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,64 +3,22 @@ class PlacesController < ApplicationController
 
   @@m = 500
 
-  def index
+   def index
      @places = Place.all
    end
 
-   def show
-   end
 
    def new
      @place = Place.new
    end
 
-   def edit
-   end
 
    def create
-     # @place = Place.create(place_params)
-     # respond_to do |format|
-     #   if @place.save
-     #     format.html { redirect_to @place, notice: 'Place was successfully created.' }
-     #     format.js {}
-     #   else
-     #     format.html { render :new }
-     #     format.js {}
-     #   end
-     # end
      PlacesJob.perform_later place_params
-     # @user = User.find_by_email(current_user.email)
-     # source = place_params
-     # puts (place_params["rating"])
-     # puts (place_params["comment_count"])
-     # rate = place_params["rating"].split("; ")
-     # rate = rate.map(&:to_f)
-     # comments_count = place_params["comment_count"].split("; ")
-     # comments_count = comments_count.map(&:to_f)
-     # @elem = 0
-     # rate.each do |elem|
-     #   @elem += elem
-     # end
-     # @elem /= rate.size
-     # # puts (@elem)
-     # rate.each_with_index do |elem, i|
-     #   @rating = (comments_count[i] / comments_count[i] + @@m) * elem + (@@m / comments_count[i] + @@m) * @elem
-     #   puts(@rating)
-     # end
-     # PlaceMailer.with(place: @result).welcome_email.deliver
    end
 
-   def update
-   end
-
-   def destroy
-   end
 
    private
-
-   # def set_place
-   #   @place = Place.find(params[:id])
-   # end
 
    def place_params
      params.require(:place).permit(:name, :address, :rating, :comment_count)
--- app/jobs/places_job.rb
+++ app/jobs/places_job.rb
@@ -1,30 +1,42 @@
 class PlacesJob < ApplicationJob
+  require 'open-uri'
+  require 'nokogiri'
   queue_as :default
 
   def perform(place_params)
-    @@m = 500
-    puts (place_params["rating"])
-    puts (place_params["comment_count"])
-    rate = place_params["rating"].split("; ")
-    rate = rate.map(&:to_f)
-    comments_count = place_params["comment_count"].split("; ")
-    comments_count = comments_count.map(&:to_f)
-    @avarage_score = 0
-    rate.each do |elem|
-      @avarage_score += elem
+    # puts(place_params["name"])
+    place = place_params["name"].split("; ")
+    # puts(place)
+    place.each_with_index do |elem, i|
+      place[i] = elem.gsub(/ /,'+')
+      @parse_url = Nokogiri::HTML(open("https://www.google.com/maps/search/#{URI.encode(place[i])}"))
+
+      puts(@parse_url)
     end
-    @avarage_score /= rate.size
-    # puts (@elem)
-    my_hash = []
-    rate.each_with_index do |elem, i|
-      @rating = (comments_count[i] / comments_count[i] + @@m) * elem + (@@m / comments_count[i] + @@m) * @avarage_score
-      # json = JSON.parse({"rating": #{@rating}}")
-      my_hash << {:rating => "#{@rating}", :comment_count => "#{comments_count[i]}", :rate => "#{elem}"}
-    end
-    places_json = my_hash.to_json
-    places_json = JSON[places_json].sort_by{ |e| e['rating'].to_i }
-    places_json = places_json.reverse
-    puts(places_json)
-    PlaceMailer.welcome_email(places_json).deliver_later
+    puts place
+    # @@m = 500
+    # puts (place_params["rating"])
+    # puts (place_params["comment_count"])
+    # rate = place_params["rating"].split("; ")
+    # rate = rate.map(&:to_f)
+    # comments_count = place_params["comment_count"].split("; ")
+    # comments_count = comments_count.map(&:to_f)
+    # @avarage_score = 0
+    # rate.each do |elem|
+    #   @avarage_score += elem
+    # end
+    # @avarage_score /= rate.size
+    # # puts (@elem)
+    # my_hash = []
+    # rate.each_with_index do |elem, i|
+    #   @rating = (comments_count[i] / comments_count[i] + @@m) * elem + (@@m / comments_count[i] + @@m) * @avarage_score
+    #   # json = JSON.parse({"rating": #{@rating}}")
+    #   my_hash << {:rating => "#{@rating}", :comment_count => "#{comments_count[i]}", :rate => "#{elem}"}
+    # end
+    # places_json = my_hash.to_json
+    # places_json = JSON[places_json].sort_by{ |e| e['rating'].to_i }
+    # places_json = places_json.reverse
+    # puts(places_json)
+    # PlaceMailer.welcome_email(places_json).deliver_later
   end
 end
