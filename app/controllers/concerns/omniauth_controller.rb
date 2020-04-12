412bec04c9db76e93bf510bafea62d209bf83065
Zippi01 <katyakovtun080619@gmail.com>
2020-04-12 20:56:04 +0300

    notice

--- app/controllers/concerns/omniauth_controller.rb
+++ app/controllers/concerns/omniauth_controller.rb
@@ -14,7 +14,7 @@ class OmniauthController < ApplicationController
   @user = User.create_from_provider_data(request.env['omniauth.auth'])
     if @user.persisted?
       redirect_to root_path
-      # flash[:notice] = 'You must activate account. We send instriction to you email'
+      flash[:notice] = 'You must activate account. We send instriction to you email(If your account is already active, please ignore this message)'
     else
       redirect_to new_user_registration_url
       flash[:notice] = 'There was a problem signing you in through Github. Please register or try signing in later.'
@@ -24,8 +24,8 @@ class OmniauthController < ApplicationController
   def google_oauth2
   @user = User.create_from_provider_data(request.env['omniauth.auth'])
     if @user.persisted?
-      sign_in_and_redirect @user
-      flash[:notice] = 'You must activate account. We send instriction to you email'
+      redirect_to root_path
+      flash[:notice] = 'You must activate account. We send instriction to you email(If your account is already active, please ignore this message)'
     else
       redirect_to new_user_registration_url
       flash[:notice] = 'There was a problem signing you in through Google. Please register or try signing in later.'
