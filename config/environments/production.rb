aa24aa0faf9a4cf542e9e60c557ad7ea37bfedf9
Zippi01 <katyakovtun080619@gmail.com>
2020-04-12 12:41:32 +0300

    fix config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,24 +1,24 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'analyzer-ghd.tk' }
   config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #     address: 'smtp.gmail.com',
-  #     port: 587,
-  #     domain: 'gmail.com',
-  #     user_name: 'testrorstore@gmail.com',
-  #     password: '63bafama',
-  #     authentication: 'login',
-  #     enable_starttls_auto: true
-  #   }
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    domain: 'yourdomain.com',
-    user_name: 'ayarem21',
-    password: '63bafamama',
-    authentication: :login,
-    enable_starttls_auto: true
-  }
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'gmail.com',
+      user_name: 'analyzer.ghd@gmail.com',
+      password: '63bafama',
+      authentication: 'login',
+      enable_starttls_auto: true
+    }
+  # ActionMailer::Base.smtp_settings = {
+  #   address: 'smtp.sendgrid.net',
+  #   port: 587,
+  #   domain: 'yourdomain.com',
+  #   user_name: 'ayarem21',
+  #   password: '63bafamama',
+  #   authentication: :login,
+  #   enable_starttls_auto: true
+  # }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
