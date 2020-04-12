f474a7fd88d31c6a6cc3ef595c401fafac08eb19
Zippi01 <katyakovtun080619@gmail.com>
2020-04-12 19:59:26 +0300

    notice

--- app/controllers/concerns/omniauth_controller.rb
+++ app/controllers/concerns/omniauth_controller.rb
@@ -14,9 +14,10 @@ class OmniauthController < ApplicationController
   @user = User.create_from_provider_data(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user
+      flash[:error] = 'You must activate account. We send instriction to you email'
     else
-      flash[:error] = 'There was a problem signing you in through Github. Please register or try signing in later.'
       redirect_to new_user_registration_url
+      flash[:error] = 'There was a problem signing you in through Github. Please register or try signing in later.'
     end
   end
 
@@ -24,14 +25,15 @@ class OmniauthController < ApplicationController
   @user = User.create_from_provider_data(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user
+      flash[:error] = 'You must activate account. We send instriction to you email'
     else
-      flash[:error] = 'There was a problem signing you in through Google. Please register or try signing in later.'
       redirect_to new_user_registration_url
+      flash[:error] = 'There was a problem signing you in through Google. Please register or try signing in later.'
     end
   end
 
   def failure
-    flash[:error] = 'There was a problem signing you in. Please register or try signing in later.'
     redirect_to root_path
+    flash[:error] = 'There was a problem signing you in. Please register or try signing in later.'
   end
 end
